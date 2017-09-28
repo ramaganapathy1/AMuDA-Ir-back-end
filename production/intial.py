8e83defea735651b1a3d343d6829ae3b9fd14daa
ramaganapathy1 <ramaganapathy96@outlook.com>
2017-09-28 17:02:16 +0530

    left overs

--- production/intial.py
+++ production/intial.py
@@ -1,5 +1,5 @@
 import os
-fd=open("start.sh","w+")
+fd=open("production/start.sh","w+")
 for i in os.listdir("/Users/orion/Documents/AMuDA-Ir-back-end/uploads"):
     if i.endswith(".pdf"):
         name="pdftotext /Users/orion/Documents/AMuDA-Ir-back-end/uploads/"+i+ " keyphrase/transcript/"+i[:-3]+"txt\n"
