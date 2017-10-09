527644b63411b204161eff15dbf1f669094d42d6
ramaganapathy1 <ramaganapathy96@outlook.com>
2017-10-09 19:59:34 +0530

    server modifications

--- production/server.py
+++ production/server.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     production.run(host="127.0.0.1",port=6000,debug=True)"""
 @app.route('/kickoff',methods=['GET'])
 def kick():
-    if(session['count']>=5):
+    if(int(session['count'])>=5):
         path = os.getcwd()
         print ("start")
         os.system('python intial.py')
