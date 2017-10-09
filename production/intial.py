ffd6bdcde65d71dcb140b0db4f9c8799adc06efe
ramaganapathy1 <ramaganapathy96@outlook.com>
2017-10-09 20:33:37 +0530

    minor changes

--- production/intial.py
+++ production/intial.py
@@ -3,7 +3,7 @@ fd=open("start.sh","w+")
 path =os.getcwd()
 path=path[:-10]
 print path
-for i in os.listdir(path+"/uploads"):
+for i in os.listdir(path+"uploads"):
     if i.endswith(".pdf"):
         name="pdftotext "+path+"uploads/"+i+" "+path+"/production/keyphrase/transcript/"+i[:-3]+"txt\n"
         print (name)
--- production/keyphrase/start.py
+++ production/keyphrase/start.py
@@ -1,7 +1,7 @@
 import os
 li = []
 path=os.getcwd()
-fd = open(path+"keyphrase/start.sh", "w+")
+fd = open(path+"/keyphrase/start.sh", "w+")
 for file in os.listdir(path+"keyphrase/transcript"):
     if file.endswith(".txt"):
         name = "./"+path+"keyphrase/keyphrase.sh a " + file + " Porter_Stemmer disable none 4 'tfidf,dispersion,cscore' all_features.tab \n \n"
