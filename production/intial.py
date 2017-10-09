9d4bb6237496b7779a8c47dcbe440496dec6d153
ramaganapathy1 <ramaganapathy96@outlook.com>
2017-10-09 19:55:30 +0530

    production server fixes

--- production/intial.py
+++ production/intial.py
@@ -1,9 +1,11 @@
 import os
 fd=open("start.sh","w+")
 path =os.getcwd()
-for i in os.listdir("../uploads"):
+path=path[:-10]
+print path
+for i in os.listdir(path+"/uploads"):
     if i.endswith(".pdf"):
-        name="pdftotext ../uploads/"+i+" "+path+"/keyphrase/transcript/"+i[:-3]+"txt\n"
+        name="pdftotext "+path+"/uploads/"+i+" "+path+"/production/keyphrase/transcript/"+i[:-3]+"txt\n"
         print (name)
         fd.write(name)
         name=""
