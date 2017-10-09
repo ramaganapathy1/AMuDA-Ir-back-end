c399d65208270d4702a4c0c18159eaac53534021
ramaganapathy1 <ramaganapathy96@outlook.com>
2017-10-09 20:18:51 +0530

    changes in paths

--- production/intial.py
+++ production/intial.py
@@ -5,7 +5,7 @@ path=path[:-10]
 print path
 for i in os.listdir(path+"/uploads"):
     if i.endswith(".pdf"):
-        name="pdftotext "+path+"/uploads/"+i+" "+path+"/production/keyphrase/transcript/"+i[:-3]+"txt\n"
+        name="pdftotext "+path+"uploads/"+i+" "+path+"/production/keyphrase/transcript/"+i[:-3]+"txt\n"
         print (name)
         fd.write(name)
         name=""
