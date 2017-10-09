eb54ff629d99a76f4533802b2000584af31bb954
ramaganapathy1 <ramaganapathy96@outlook.com>
2017-10-09 20:16:45 +0530

    left over files

--- production/keyphrase/start.py
+++ production/keyphrase/start.py
@@ -1,8 +1,9 @@
 import os
 li = []
-fd = open("keyphrase/start.sh", "w+")
-for file in os.listdir("keyphrase/transcript"):
+path=os.getcwd()
+fd = open(path+"keyphrase/start.sh", "w+")
+for file in os.listdir(path+"keyphrase/transcript"):
     if file.endswith(".txt"):
-        name = "./keyphrase/keyphrase.sh a " + file + " Porter_Stemmer disable none 4 'tfidf,dispersion,cscore' all_features.tab \n \n"
+        name = "./"+path+"keyphrase/keyphrase.sh a " + file + " Porter_Stemmer disable none 4 'tfidf,dispersion,cscore' all_features.tab \n \n"
         print(name)
         fd.write(name)
--- production/keyphrase/start.sh
+++ production/keyphrase/start.sh
@@ -1,2 +1,10 @@
 ./keyphrase/keyphrase.sh a 9444370166-7408-7713-1-PB.txt Porter_Stemmer disable none 4 'tfidf,dispersion,cscore' all_features.tab 
  
+./keyphrase/keyphrase.sh a 9444370166-CiteSeer-Anautomaticcitationindexingsystem.txt Porter_Stemmer disable none 4 'tfidf,dispersion,cscore' all_features.tab 
+ 
+./keyphrase/keyphrase.sh a 9444370166-JV.txt Porter_Stemmer disable none 4 'tfidf,dispersion,cscore' all_features.tab 
+ 
+./keyphrase/keyphrase.sh a 9444370166-KRDocumentation.txt Porter_Stemmer disable none 4 'tfidf,dispersion,cscore' all_features.tab 
+ 
+./keyphrase/keyphrase.sh a 9444370166-SFXavier-enhancingstatisticalsemanticnetworkswithconceptualheirarchies.txt Porter_Stemmer disable none 4 'tfidf,dispersion,cscore' all_features.tab 
+ 
